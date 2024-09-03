08e5fcbb280686ef38430a25c056e12735d124d8
ydideh810 <yd.baller810@gmail.com>
2024-09-03 11:49:22 -0400

    Update page.tsx

--- app/[locale]/privacy/page.tsx
+++ app/[locale]/privacy/page.tsx
@@ -3,7 +3,7 @@ import WebsiteLayout from '@/layouts/WebsiteLayout'
 
 export const metadata = {
   title: 'Privacy Policy',
-  description: 'See how Findto respects your privacy and the users data.',
+  description: 'See how Nid-Search respects your privacy and the users data.',
 }
 
 export default function PrivacyPage() {
@@ -23,35 +23,35 @@ export default function PrivacyPage() {
             <h2>1. Privacy-First</h2>
 
             <p>
-              Findto are built on privacy! We respect your privacy regarding any
+              Nid-Search are built on privacy! We respect your privacy regarding any
               information we may collect from you across our services at
-              internet domain <a href="http://findto.app">http://findto.app</a>{' '}
-              and communications ("Findto").
+              internet domain <a href="http://Nid-Search.app">http://Nid-Search.app</a>{' '}
+              and communications ("Nid-Search").
             </p>
 
             <p>
-              Findto don't store any user searches. Because was designed for it.
-              Findto mission is encourage people choice a healthy internet.
+              Nid-Search don't store any user searches. Because was designed for it.
+              Nid-Search mission is encourage people choice a healthy internet.
               External search provider services may collect and store your
-              personal data. Thinking about it, Findto displays a "Privacy level
+              personal data. Thinking about it, Nid-Search displays a "Privacy level
               widget" for every search provider, whenever available.
             </p>
 
             <p>
-              Findto endorses user control over their data. This privacy policy
+              Nid-Search endorses user control over their data. This privacy policy
               have accordance with the Brazil General Data Protection Law
               (LGPD), European Union General Data Protection Regulation (GDPR)
               and similar legislation's.
             </p>
 
             <p>
-              Using Findto you are agreeing to be bound by this privacy policy.{' '}
+              Using Nid-Search you are agreeing to be bound by this privacy policy.{' '}
             </p>
 
             <h2>2. Automatically Collected Information</h2>
 
             <p>
-              Findto automatically collect and store minimal public information
+              Nid-Search automatically collect and store minimal public information
               (non personal data) with purpose to provide web search
               functionality. Such as pages that you access, location (country
               and city), device specs (operational system and browser), internet
@@ -62,7 +62,7 @@ export default function PrivacyPage() {
             <h2>3. Information that You Provide</h2>
 
             <p>
-              Findto only receives and stores identifiable information (personal
+              Nid-Search only receives and stores identifiable information (personal
               data) for those who chose identify themselves. Data that you enter
               on contact or feedback forms, donations platforms or give in any
               other way, including but not limited to: your name, email address
@@ -70,7 +70,7 @@ export default function PrivacyPage() {
             </p>
 
             <p>
-              Findto only ask for personal information when we truly need it to
+              Nid-Search only ask for personal information when we truly need it to
               provide a service to you. We collect it by fair and lawful means,
               with your knowledge and consent. We also let you know why we're
               collecting it and how it will be used.
@@ -78,7 +78,7 @@ export default function PrivacyPage() {
 
             <h2>4. Information Using</h2>
 
-            <p>Findto uses your information only with the purposes of: </p>
+            <p>Nid-Search uses your information only with the purposes of: </p>
 
             <p>
               a) communicate with you to understand your feedback, respond to
@@ -99,7 +99,7 @@ export default function PrivacyPage() {
             <h2>5. Cookies and Tracking Technologies</h2>
 
             <p>
-              Findto and it's related email communications, use cookies and
+              Nid-Search and it's related email communications, use cookies and
               similar technologies, such as cache and local storage, only to
               execute app functionalities, like to recognize when you view or
               return to a page.{' '}
@@ -127,7 +127,7 @@ export default function PrivacyPage() {
                 rel="noopener noreferrer">
                 Microsoft Clarity
               </a>{' '}
-              services are used to optimize Findto, and can put their own
+              services are used to optimize Nid-Search, and can put their own
               cookies in your browser. This policy not covers use of cookies by
               third parties. Click on the links above to read their privacy
               policies.{' '}
@@ -142,7 +142,7 @@ export default function PrivacyPage() {
             <h2>6. Information Sharing</h2>
 
             <p>
-              Findto do not share any personally identifying information with
+              Nid-Search do not share any personally identifying information with
               third-parties. But for technical reasons and unintentionally, it
               may share with the services listed above and when redirecting the
               user to a search provider. These entities may have access to
@@ -152,7 +152,7 @@ export default function PrivacyPage() {
             <h2>7. Information Store and Protection</h2>
 
             <p>
-              Findto only retain collected information for as long as necessary
+              Nid-Search only retain collected information for as long as necessary
               to provide to you the requested service. The data we store, we'll
               protect within commercially acceptable means to prevent
               unauthorized access. You are free to refuse our request for your
@@ -163,7 +163,7 @@ export default function PrivacyPage() {
             <h2>8. Opting Out</h2>
 
             <p>
-              Findto strongly encourages you take a look on third parties
+              Nid-Search strongly encourages you take a look on third parties
               privacy policies linked above and of every search provider, to
               know how this services are handling your data and how proceed to
               opt out of them individually.
@@ -172,12 +172,12 @@ export default function PrivacyPage() {
             <h2>9. External Services</h2>
 
             <p>
-              Findto redirects and contain links to external websites and
+              Nid-Search redirects and contain links to external websites and
               applications just for your convenience of search functionality.
-              Please be aware, that Findto not endorse, have no control over the
+              Please be aware, that Nid-Search not endorse, have no control over the
               content and practices of any third party internet resources, and
               cannot accept liability or responsibility for their respective
-              privacy policies, that are not operated by Findto. We advised you
+              privacy policies, that are not operated by Nid-Search. We advised you
               to review the privacy policy of every them, as possible.
             </p>
 
@@ -185,8 +185,8 @@ export default function PrivacyPage() {
 
             <p>
               Note that this policy can be revised at any time without notice.
-              However, significant changes will be announced on Findto's
-              communication channels. Your continued use of Findto, following
+              However, significant changes will be announced on Nid-Search's
+              communication channels. Your continued use of Nid-Search, following
               the posting of changes, will be regarded as acceptance of these
               changes.
             </p>
@@ -195,7 +195,7 @@ export default function PrivacyPage() {
 
             <p>
               If you have any questions or suggestions, please contact us at
-              email legal@findto.app. Happy searches!
+              email legal@Nid-Search.app. Happy searches!
             </p>
 
             <p>Effective: August 1, 2024.</p>
